9a8f336e8d3fa53f8b5ef197981f3a177aa70c0b
Iamneo <techadmin@iamneo.ai>
2023-10-05 05:07:12 +0000

    Backup Commit

--- springapp/src/controller/MedicineController.java
+++ springapp/src/controller/MedicineController.java
@@ -1,22 +1,13 @@
-package com.example.controller;
-import org.springframework.web.bind.annotation.*;
-import com.example.model.Medicine;
-import java.util.HashMap;
-import java.util.Map;
-@RestController
-@RequestMapping("/medicines")
 public class MedicineController {
     private Map<Integer,Medicine>medicineMap=new HashMap<>();
-    @PostMapping
-    public boolean addMedicine(@RequestBody Medicine medicine){
+    public boolean addMedicine(Medicine medicine){
         if(medicineMap.containsKey(medicine.getMedicineId())){
             return false;
         }
         medicineMap.put(medicine.getMedicineId(),medicine);
         return true;
     }
-    @PutMapping("/{medicineId}")
-    public Medicine updateMedicine(@PathVariable int medicineId,@RequestBody Medicine updatedMedicine){
+    public Medicine updateMedicine(int medicineId,Medicine updatedMedicine){
         if (medicineMap.containsKey(medicineId)) {
             Medicine medicine=medicineMp.get(medicineId);
             medicine.setMedicineName(updatedMedicine.getMedicineName());
